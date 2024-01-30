e415f3456444bdfc96f19292132c5e2a1ff1fe94
Shougot mollik <shougotmollik@gmail.com>
2024-01-31 01:03:25 +0600

    Implementation

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import LoginScreen from "./Apps/Screens/LoginScreen";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LoginScreen />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? 35 : 0,
   },
 });
