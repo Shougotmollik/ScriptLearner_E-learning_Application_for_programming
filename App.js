a277c29f00cac939340128e5d6598ef80d039ab8
Shougot mollik <shougotmollik@gmail.com>
2024-01-30 15:11:51 +0600

    start new project

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Login from "./App/pages/Login";
 
 export default function App() {
   return (
-    <View>
-      <Login />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -14,5 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
