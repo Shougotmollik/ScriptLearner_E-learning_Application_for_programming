7928fa42769083adf312c7c921013abb89a9c26e
Shougot mollik <shougotmollik@gmail.com>
2024-02-01 15:08:00 +0600

    complete the app

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { Platform, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { createContext, useEffect, useState } from "react";
 import LoginScreen from "./Apps/Screens/LoginScreen";
+import { client } from "./Apps/Utils/kindeConfig";
+import { NavigationContainer } from "@react-navigation/native";
+import TabNavigations from "./Apps/Navigations/TabNavigations";
 
+export const AuthContext = createContext();
 const App = () => {
+  const [auth, setAuth] = useState(false);
+  const checkAuthenticate = async () => {
+    // Using `isAuthenticated` to check if the user is authenticated or not
+    if (await client.isAuthenticated) {
+      const userProfile = await client.getUserDetails();
+      setAuth(true);
+      // Need to implement, e.g: call an api, etc...
+    } else {
+      setAuth(false);
+      // Need to implement, e.g: redirect user to sign in, etc..
+    }
+  };
+
+  useEffect(() => {
+    checkAuthenticate();
+  }, [auth]);
   return (
     <View style={styles.container}>
-      <LoginScreen />
+      <AuthContext.Provider
+        value={{
+          auth,
+          setAuth,
+        }}
+      >
+        {/* <LoginScreen /> */}
+        <NavigationContainer>
+          {auth ? <TabNavigations /> : <LoginScreen />}
+        </NavigationContainer>
+      </AuthContext.Provider>
     </View>
   );
 };
@@ -14,6 +44,7 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     paddingTop: Platform.OS === "android" ? 35 : 0,
   },
 });
